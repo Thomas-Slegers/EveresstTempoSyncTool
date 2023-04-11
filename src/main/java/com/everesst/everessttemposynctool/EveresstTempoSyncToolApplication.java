134145506a41ff1c1b19ddf5c66585a5dd3b9fb7
Thomas-Slegers <Thomsleg@hotmail.com>
2023-04-11 14:05:23 +0200

    Init: Start java spring boot with gradle & spring web and h2 database dependencies

---
+++ src/main/java/com/everesst/everessttemposynctool/EveresstTempoSyncToolApplication.java
@@ -0,0 +1,13 @@
+package com.everesst.everessttemposynctool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveresstTempoSyncToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EveresstTempoSyncToolApplication.class, args);
+	}
+
+}
