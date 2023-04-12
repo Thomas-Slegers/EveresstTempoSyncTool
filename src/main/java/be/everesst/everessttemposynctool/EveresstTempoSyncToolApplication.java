ca823de6608b7fafced5d3ad8fd332c8b251643f
Thomas-Slegers <Thomsleg@hotmail.com>
2023-04-12 10:51:48 +0200

    Add Unversioned files: Application & test files

---
+++ src/main/java/be/everesst/everessttemposynctool/EveresstTempoSyncToolApplication.java
@@ -0,0 +1,13 @@
+package be.everesst.everessttemposynctool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveresstTempoSyncToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EveresstTempoSyncToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/everesst/everessttemposynctool/EveresstTempoSyncToolApplicationTests.java
@@ -0,0 +1,13 @@
+package be.everesst.everessttemposynctool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EveresstTempoSyncToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
