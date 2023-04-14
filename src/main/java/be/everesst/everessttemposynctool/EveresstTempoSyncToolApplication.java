54bcc28c646adbc1816e9c185fccc5e9bd3ac0aa
Thomas-Slegers <Thomsleg@hotmail.com>
2023-04-14 13:05:45 +0200

    Application: Refactoring name variables error jpa entities to sync entities

--- src/main/java/be/everesst/everessttemposynctool/EveresstTempoSyncToolApplication.java
+++ src/main/java/be/everesst/everessttemposynctool/EveresstTempoSyncToolApplication.java
@@ -21,12 +21,12 @@ public class EveresstTempoSyncToolApplication {
     @Bean
     CommandLineRunner init(SyncRepository syncRepository) {
         return args -> {
-            ArrayList<SyncEntity> errorJpaEntities = new ArrayList<>();
-            errorJpaEntities.add(new SyncEntity("Testing", LocalDate.now(), "9005026", "John", "AD", "LMAC000.003", 1, 1, "Less than 40h logged in week 1", "Fill till you have.."));
-            errorJpaEntities.add(new SyncEntity("Ravi", LocalDate.now(), "I099716", "Julie", "AD", "LMAC000.003", 0.5, 1, "Less than 40h logged in week 2", "Fill till you have.."));
-            errorJpaEntities.add(new SyncEntity("Ravi", LocalDate.now(), "9005026", "Jennifer", "AD", "LMAC000.003", 5, 2, "sum(camisHours) > sum(tempoHours) ...", "The tools differ from values..."));
-            errorJpaEntities.add(new SyncEntity("Sherpa", LocalDate.now(), "9005026", "Roel", "AD", " 2023-03-17 with workorder LMAC...", 6, 3, "", "Workorder ..."));
-            Stream.of(errorJpaEntities).forEach(syncRepository::saveAll);
+            ArrayList<SyncEntity> syncEntities = new ArrayList<>();
+            syncEntities.add(new SyncEntity("Testing", LocalDate.now(), "9005026", "John", "AD", "LMAC000.003", 1, 1, "Less than 40h logged in week 1", "Fill till you have.."));
+            syncEntities.add(new SyncEntity("Ravi", LocalDate.now(), "I099716", "Julie", "AD", "LMAC000.003", 0.5, 1, "Less than 40h logged in week 2", "Fill till you have.."));
+            syncEntities.add(new SyncEntity("Ravi", LocalDate.now(), "9005026", "Jennifer", "AD", "LMAC000.003", 5, 2, "sum(camisHours) > sum(tempoHours) ...", "The tools differ from values..."));
+            syncEntities.add(new SyncEntity("Sherpa", LocalDate.now(), "9005026", "Roel", "AD", " 2023-03-17 with workorder LMAC...", 6, 3, "", "Workorder ..."));
+            Stream.of(syncEntities).forEach(syncRepository::saveAll);
             syncRepository.findAll().forEach(syncEntity -> System.out.println(syncEntity.toString()));
         };
     }
