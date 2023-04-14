a48aee425d5724a9d435388e4092929bbe8d792d
Thomas-Slegers <Thomsleg@hotmail.com>
2023-04-14 16:09:28 +0200

    Application: Added extra uuid in the example data

--- src/main/java/be/everesst/everessttemposynctool/EveresstTempoSyncToolApplication.java
+++ src/main/java/be/everesst/everessttemposynctool/EveresstTempoSyncToolApplication.java
@@ -22,12 +22,13 @@ public class EveresstTempoSyncToolApplication {
     @Bean
     CommandLineRunner init(SyncRepository syncRepository) {
         return args -> {
-            UUID syncTableUUID = UUID.randomUUID();
+            UUID syncTableUUID1 = UUID.fromString("c29c801d-f9b3-4c90-b99c-9c5c2d4d4601");
+            UUID syncTableUUID2 = UUID.fromString("f9b3-4c90-b99c-9c5c2d4d4601-c29c801d");
             ArrayList<SyncEntity> syncEntities = new ArrayList<>();
-            syncEntities.add(new SyncEntity(syncTableUUID, "Testing", LocalDate.now(), "9005026", "John", "AD", "LMAC000.003", 1, 1, "Less than 40h logged in week 1", "Fill till you have.."));
-            syncEntities.add(new SyncEntity(syncTableUUID, "Ravi", LocalDate.now(), "I099716", "Julie", "AD", "LMAC000.003", 0.5, 1, "Less than 40h logged in week 2", "Fill till you have.."));
-            syncEntities.add(new SyncEntity(syncTableUUID, "Ravi", LocalDate.now(), "9005026", "Jennifer", "AD", "LMAC000.003", 5, 2, "sum(camisHours) > sum(tempoHours) ...", "The tools differ from values..."));
-            syncEntities.add(new SyncEntity(syncTableUUID, "Sherpa", LocalDate.now(), "9005026", "Roel", "AD", " 2023-03-17 with workorder LMAC...", 6, 6, "", "Workorder ..."));
+            syncEntities.add(new SyncEntity(syncTableUUID1, "Testing", LocalDate.now(), "9005026", "John", "AD", "LMAC000.003", 1, 1, "Less than 40h logged in week 1", "Fill till you have.."));
+            syncEntities.add(new SyncEntity(syncTableUUID1, "Ravi", LocalDate.now(), "I099716", "Julie", "AD", "LMAC000.003", 0.5, 1, "Less than 40h logged in week 2", "Fill till you have.."));
+            syncEntities.add(new SyncEntity(syncTableUUID2, "Ravi", LocalDate.now(), "9005026", "Jennifer", "AD", "LMAC000.003", 5, 2, "sum(camisHours) > sum(tempoHours) ...", "The tools differ from values..."));
+            syncEntities.add(new SyncEntity(syncTableUUID2, "Sherpa", LocalDate.now(), "9005026", "Roel", "AD", " 2023-03-17 with workorder LMAC...", 6, 6, "", "Workorder ..."));
             Stream.of(syncEntities).forEach(syncRepository::saveAll);
             syncRepository.findAll().forEach(syncEntity -> System.out.println(syncEntity.toString()));
         };
