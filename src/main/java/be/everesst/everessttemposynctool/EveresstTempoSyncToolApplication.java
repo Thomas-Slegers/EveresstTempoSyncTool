1261431875330f4ff06237361485b2d0b2f70f2a
Thomas-Slegers <Thomsleg@hotmail.com>
2023-04-14 14:40:01 +0200

    Application: Refactoring sync to error & example data change

--- src/main/java/be/everesst/everessttemposynctool/EveresstTempoSyncToolApplication.java
+++ src/main/java/be/everesst/everessttemposynctool/EveresstTempoSyncToolApplication.java
@@ -1,7 +1,7 @@
 package be.everesst.everessttemposynctool;
 
-import be.everesst.everessttemposynctool.model.error.SyncEntity;
-import be.everesst.everessttemposynctool.model.error.SyncRepository;
+import be.everesst.everessttemposynctool.model.sync.SyncEntity;
+import be.everesst.everessttemposynctool.model.sync.SyncRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -27,7 +27,7 @@ public class EveresstTempoSyncToolApplication {
             syncEntities.add(new SyncEntity(syncTableUUID, "Testing", LocalDate.now(), "9005026", "John", "AD", "LMAC000.003", 1, 1, "Less than 40h logged in week 1", "Fill till you have.."));
             syncEntities.add(new SyncEntity(syncTableUUID, "Ravi", LocalDate.now(), "I099716", "Julie", "AD", "LMAC000.003", 0.5, 1, "Less than 40h logged in week 2", "Fill till you have.."));
             syncEntities.add(new SyncEntity(syncTableUUID, "Ravi", LocalDate.now(), "9005026", "Jennifer", "AD", "LMAC000.003", 5, 2, "sum(camisHours) > sum(tempoHours) ...", "The tools differ from values..."));
-            syncEntities.add(new SyncEntity(syncTableUUID, "Sherpa", LocalDate.now(), "9005026", "Roel", "AD", " 2023-03-17 with workorder LMAC...", 6, 3, "", "Workorder ..."));
+            syncEntities.add(new SyncEntity(syncTableUUID, "Sherpa", LocalDate.now(), "9005026", "Roel", "AD", " 2023-03-17 with workorder LMAC...", 6, 6, "", "Workorder ..."));
             Stream.of(syncEntities).forEach(syncRepository::saveAll);
             syncRepository.findAll().forEach(syncEntity -> System.out.println(syncEntity.toString()));
         };
