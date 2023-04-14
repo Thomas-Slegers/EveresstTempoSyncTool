96ad00a123a0c85b3b98e82756fa13dea4f5ddbc
Thomas-Slegers <Thomsleg@hotmail.com>
2023-04-14 10:35:09 +0200

    SyncInputEntity: Created sync input entity

---
+++ src/main/java/be/everesst/everessttemposynctool/model/error/SyncInputEntity.java
@@ -0,0 +1,41 @@
+package be.everesst.everessttemposynctool.model.error;
+
+import jakarta.persistence.*;
+
+import java.io.File;
+
+@Entity
+public class SyncInputEntity {
+
+    @Id
+    private File file;
+
+    private String operation;
+
+    private String baseUrl;
+
+    private String clientId;
+
+    private String clientSecret;
+
+
+    protected SyncInputEntity() {
+    }
+
+    public SyncInputEntity(File file, String operation, String baseUrl, String clientId, String clientSecret) {
+        this.file = file;
+        this.operation = operation;
+        this.baseUrl = baseUrl;
+        this.clientId = clientId;
+        this.clientSecret = clientSecret;
+    }
+
+    @Override
+    public String toString() {
+        return "SyncInputJpaEntity{" + "file=" + file + ", operation='" + operation + '\'' + ", baseUrl='" + baseUrl + '\'' + ", clientId='" + clientId + '\'' + ", clientSecret='" + clientSecret + '\'' + '}';
+    }
+
+    public File getFile() {
+        return file;
+    }
+}
