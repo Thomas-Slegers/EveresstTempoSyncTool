22934a28b3845e7a9669a48e5ace1a7f8cb8cdb2
Thomas-Slegers <Thomsleg@hotmail.com>
2023-04-19 16:12:04 +0200

    SyncController: Added data transfer fro; camis-api but manually chosen uuid.

--- src/main/java/be/everesst/everessttemposynctool/controller/SyncController.java
+++ src/main/java/be/everesst/everessttemposynctool/controller/SyncController.java
@@ -3,9 +3,11 @@ package be.everesst.everessttemposynctool.controller;
 import be.everesst.everessttemposynctool.model.sync.entities.SyncRecordEntity;
 import be.everesst.everessttemposynctool.model.sync.entities.SyncInputEntity;
 import be.everesst.everessttemposynctool.service.SyncRecordService;
+import be.everesst.everessttemposynctool.service.SyncResultService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
@@ -16,6 +18,8 @@ import java.util.UUID;
 public class SyncController {
     @Autowired
     private SyncRecordService syncRecordService;
+    @Autowired
+    private SyncResultService syncResultService;
 
     @GetMapping(value = "/sync/{syncTableUUID}/{id}")
     public SyncRecordEntity findSyncEntitiesBySyncTableUUIDAndId(@PathVariable UUID syncTableUUID, @PathVariable Long id) {
@@ -33,8 +37,18 @@ public class SyncController {
     }
 
     @PostMapping("/camis-api/sync-response")
-    public void findAllSyncResponses(@RequestBody Map<String, Object> payload) {
+    public void findAllSyncResponses(@RequestBody Map<String, Map<String, String>> payload) {
+        UUID syncResultUUID1 = UUID.fromString("c29c801d-f9b3-4c90-b99c-9c5c2d4d4601");
+        syncResultService.createSyncResult(syncResultUUID1);
         System.out.println(payload);
+        payload.forEach((key, value) -> {
+            syncRecordService.saveRecord(syncResultUUID1, new SyncRecordEntity(Long.parseLong(key),
+                    value.get("message"),
+                    value.get("employeeName"),
+                    LocalDate.parse(value.get("startDate")),
+                    Double.parseDouble(value.get("hoursLogged")),
+                    value.get("workOrder")));
+        });
     }
 }
 
