2d81d75064933d8e36da6a663ae9751ec032c196
Thomas-Slegers <Thomsleg@hotmail.com>
2023-04-19 16:12:45 +0200

    Application: Removed example data

--- src/main/java/be/everesst/everessttemposynctool/EveresstTempoSyncToolApplication.java
+++ src/main/java/be/everesst/everessttemposynctool/EveresstTempoSyncToolApplication.java
@@ -1,15 +1,7 @@
 package be.everesst.everessttemposynctool;
 
-import be.everesst.everessttemposynctool.model.sync.entities.SyncRecordEntity;
-import be.everesst.everessttemposynctool.service.SyncRecordService;
-import be.everesst.everessttemposynctool.service.SyncResultService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.time.LocalDate;
-import java.util.UUID;
 
 @SpringBootApplication
 public class EveresstTempoSyncToolApplication {
@@ -18,16 +10,16 @@ public class EveresstTempoSyncToolApplication {
         SpringApplication.run(EveresstTempoSyncToolApplication.class, args);
     }
 
-    @Bean
-    CommandLineRunner init(SyncResultService syncResultService, SyncRecordService syncRecordService) {
-        return args -> {
-            UUID syncResultUUID1 = UUID.fromString("c29c801d-f9b3-4c90-b99c-9c5c2d4d4601");
-            syncResultService.createSyncResult(syncResultUUID1);
-            syncRecordService.saveRecord(syncResultUUID1, new SyncRecordEntity(1, "Less than 40h logged in week 2", "John", LocalDate.now(), 40, "LMAC000.001"));
-            syncRecordService.saveRecord(syncResultUUID1, new SyncRecordEntity(2, "Less than 40h logged in week 2", "John", LocalDate.now(), 40, "LMAC000.002"));
-            syncRecordService.saveRecord(syncResultUUID1, new SyncRecordEntity(3, "Less than 40h logged in week 2", "John", LocalDate.now(), 40, "LMAC000.003"));
-            syncRecordService.saveRecord(syncResultUUID1, new SyncRecordEntity(4, "Less than 40h logged in week 2", "John", LocalDate.now(), 40, "LMAC000.004"));
-            syncRecordService.findAllSyncRecordsByUUID(syncResultUUID1).forEach(syncRecord -> System.out.println(syncRecord.toString()));
-        };
-    }
+//    @Bean
+//    CommandLineRunner init(SyncResultService syncResultService, SyncRecordService syncRecordService) {
+//        return args -> {
+//            UUID syncResultUUID1 = UUID.fromString("c29c801d-f9b3-4c90-b99c-9c5c2d4d4601");
+//            syncResultService.createSyncResult(syncResultUUID1);
+//            syncRecordService.saveRecord(syncResultUUID1, new SyncRecordEntity(1, "Less than 40h logged in week 2", "John", LocalDate.now(), 40, "LMAC000.001"));
+//            syncRecordService.saveRecord(syncResultUUID1, new SyncRecordEntity(2, "Less than 40h logged in week 2", "John", LocalDate.now(), 40, "LMAC000.002"));
+//            syncRecordService.saveRecord(syncResultUUID1, new SyncRecordEntity(3, "Less than 40h logged in week 2", "John", LocalDate.now(), 40, "LMAC000.003"));
+//            syncRecordService.saveRecord(syncResultUUID1, new SyncRecordEntity(4, "Less than 40h logged in week 2", "John", LocalDate.now(), 40, "LMAC000.004"));
+//            syncRecordService.findAllSyncRecordsByUUID(syncResultUUID1).forEach(syncRecord -> System.out.println(syncRecord.toString()));
+//        };
+//    }
 }
