dd79bbf103511a928f2f508c31e231b4922a489a
Thomas-Slegers <Thomsleg@hotmail.com>
2023-04-25 13:45:23 +0200

    SyncController: SyncInputService added & postMapping old camis-api removed

--- src/main/java/be/everesst/everessttemposynctool/controller/SyncController.java
+++ src/main/java/be/everesst/everessttemposynctool/controller/SyncController.java
@@ -2,14 +2,12 @@ package be.everesst.everessttemposynctool.controller;
 
 import be.everesst.everessttemposynctool.model.sync.entities.SyncRecordEntity;
 import be.everesst.everessttemposynctool.model.sync.entities.SyncInputEntity;
+import be.everesst.everessttemposynctool.service.SyncInputService;
 import be.everesst.everessttemposynctool.service.SyncRecordService;
-import be.everesst.everessttemposynctool.service.SyncResultService;
-import com.basic.Main;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
-import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 
@@ -20,11 +18,10 @@ public class SyncController {
     @Autowired
     private SyncRecordService syncRecordService;
     @Autowired
-    private SyncResultService syncResultService;
+    private SyncInputService syncInputService;
 
     @GetMapping(value = "/sync/{syncTableUUID}/{id}")
     public SyncRecordEntity findSyncEntitiesBySyncTableUUIDAndId(@PathVariable UUID syncTableUUID, @PathVariable Long id) {
-        Main.main(null);
         return syncRecordService.findSyncRecordByUUIDAndSyncRecordId(syncTableUUID, id);
     }
 
@@ -35,22 +32,7 @@ public class SyncController {
 
     @PostMapping(value = "/input")
     public void startSync(@RequestBody SyncInputEntity syncInputEntity) {
-        System.out.println(syncInputEntity.getFile().toString());
-    }
-
-    @PostMapping("/camis-api/sync-response")
-    public void findAllSyncResponses(@RequestBody Map<String, Map<String, String>> payload) {
-        UUID syncResultUUID1 = UUID.fromString("c29c801d-f9b3-4c90-b99c-9c5c2d4d4601");
-        syncResultService.createSyncResult(syncResultUUID1);
-        System.out.println(payload);
-        payload.forEach((key, value) -> {
-            syncRecordService.saveRecord(syncResultUUID1, new SyncRecordEntity(Long.parseLong(key),
-                    value.get("message"),
-                    value.get("employeeName"),
-                    LocalDate.parse(value.get("startDate")),
-                    Double.parseDouble(value.get("hoursLogged")),
-                    value.get("workOrder")));
-        });
+        syncInputService.startCamisApi(syncInputEntity);
     }
 }
 
