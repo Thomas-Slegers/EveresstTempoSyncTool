6452a1c80475a4ba25819e173a02650154ff4812
Thomas-Slegers <Thomsleg@hotmail.com>
2023-05-02 11:25:49 +0200

    Added ScanBasePackages: com.cegeka & be.*

--- src/main/java/be/everesst/everessttemposynctool/EveresstTempoSyncToolApplication.java
+++ src/main/java/be/everesst/everessttemposynctool/EveresstTempoSyncToolApplication.java
@@ -3,7 +3,7 @@ package be.everesst.everessttemposynctool;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.cegeka.horizon.camis.*", "be.*"})
 public class EveresstTempoSyncToolApplication {
 
     public static void main(String[] args) {
