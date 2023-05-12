adb0d6e365426997d845e24f7ca5e74ee937386c
Thomas-Slegers <Thomsleg@hotmail.com>
2023-05-12 11:06:09 +0200

    Version 27: SyncInputService remove SyncDay en Record Service en depend on SyncResultService

--- src/main/java/be/everesst/everessttemposynctool/service/SyncInputService.java
+++ src/main/java/be/everesst/everessttemposynctool/service/SyncInputService.java
@@ -1,6 +1,7 @@
 package be.everesst.everessttemposynctool.service;
 
 import be.everesst.everessttemposynctool.model.sync.entities.SyncInputEntity;
+import be.everesst.everessttemposynctool.model.sync.entities.SyncResultEntity;
 import com.cegeka.horizon.camis.sync_logger.model.SyncResult;
 import com.cegeka.horizon.camis.sync_timesheet.service.SyncTimesheetService;
 import com.cegeka.horizon.camis.sync_timesheet.csv.HoursLoggedCsvReader;
@@ -20,29 +21,27 @@ public class SyncInputService {
 
     private static final String baseURL = "https://gw.api.cegeka.com/1/erp/camis/v1/prd/";
 
-    SyncDayService syncDayService;
-
-    SyncRecordService syncRecordService;
-
     SyncResultService syncResultService;
 
     SyncTimesheetService syncTimesheetService;
 
-    public SyncInputService(SyncDayService syncDayService, SyncTimesheetService syncTimesheetService, SyncResultService syncResultService, SyncRecordService syncRecordService) {
-        this.syncDayService = syncDayService;
+    public SyncInputService(SyncTimesheetService syncTimesheetService, SyncResultService syncResultService) {
         this.syncTimesheetService = syncTimesheetService;
         this.syncResultService = syncResultService;
-        this.syncRecordService = syncRecordService;
-
     }
 
     public void startCamisApi(SyncInputEntity syncInputEntity) {
         WebClient webClient = getWebClient(baseURL, syncInputEntity.clientId(), syncInputEntity.clientSecret());
         List<Employee> employees = new HoursLoggedCsvReader(syncInputEntity.inputStream()).readCsv();
         SyncResult syncResult = syncTimesheetService.sync(webClient, employees);
-        syncResultService.createSyncResult(syncInputEntity.syncResultUUID());
-        syncRecordService.saveSyncRecords(syncInputEntity.syncResultUUID(), syncRecordsToSyncRecordEntities(syncResult.getSyncRecords()));
-        syncDayService.saveSyncDays(syncInputEntity.syncResultUUID(), syncDaysToSyncDaysEntities(syncResult.getSyncDays()));
+        saveSyncResultWithData(syncInputEntity, syncResult);
+    }
+
+    private void saveSyncResultWithData(SyncInputEntity syncInputEntity, SyncResult syncResult){
+        SyncResultEntity syncResultEntity = new SyncResultEntity(syncInputEntity.syncResultUUID());
+        syncResultEntity.setSyncRecords(syncRecordsToSyncRecordEntities(syncResult.getSyncRecords()));
+        syncResultEntity.setSyncDays(syncDaysToSyncDaysEntities(syncResult.getSyncDays()));
+        syncResultService.save(syncResultEntity);
     }
 }
 
