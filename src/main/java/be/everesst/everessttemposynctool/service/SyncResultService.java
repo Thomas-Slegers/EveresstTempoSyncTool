08e4022ed0101e92e1f84a37de37aafd4f351bab
Thomas-Slegers <Thomsleg@hotmail.com>
2023-05-12 11:08:03 +0200

    Version 27: SyncResultService added save to Repository and getSyncResultEntityByUUID

--- src/main/java/be/everesst/everessttemposynctool/service/SyncResultService.java
+++ src/main/java/be/everesst/everessttemposynctool/service/SyncResultService.java
@@ -3,7 +3,6 @@ package be.everesst.everessttemposynctool.service;
 import be.everesst.everessttemposynctool.model.sync.entities.SyncResultEntity;
 import be.everesst.everessttemposynctool.model.sync.repositories.SyncResultRepository;
 import org.springframework.stereotype.Service;
-
 import java.util.UUID;
 
 @Service
@@ -15,7 +14,11 @@ public class SyncResultService {
         this.syncResultRepository = syncResultRepository;
     }
 
-    public void createSyncResult(UUID syncResultUUID) {
-        syncResultRepository.save(new SyncResultEntity(syncResultUUID));
+    public void save(SyncResultEntity syncResultEntity) {
+        syncResultRepository.save(syncResultEntity);
+    }
+
+    public SyncResultEntity getSyncResultEntityByUUID(UUID syncResultUUID){
+        return syncResultRepository.findSyncResultEntityBySyncResultUUID(syncResultUUID);
     }
 }
