f00c29cd896b500644bc094b33a92ad71f70f259
Thomas-Slegers <Thomsleg@hotmail.com>
2023-05-12 11:08:29 +0200

    Version 27: SyncTransformEntities List to Set modified

--- src/main/java/be/everesst/everessttemposynctool/service/SyncTransformEntities.java
+++ src/main/java/be/everesst/everessttemposynctool/service/SyncTransformEntities.java
@@ -4,21 +4,21 @@ import be.everesst.everessttemposynctool.model.sync.entities.SyncDayEntity;
 import be.everesst.everessttemposynctool.model.sync.entities.SyncRecordEntity;
 import com.cegeka.horizon.camis.sync_logger.model.SyncDay;
 import com.cegeka.horizon.camis.sync_logger.model.syncrecord.SyncRecord;
-
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class SyncTransformEntities {
-    public static List<SyncRecordEntity> syncRecordsToSyncRecordEntities(List<SyncRecord> syncRecords) {
-        List<SyncRecordEntity> syncRecordEntities = new ArrayList<>();
+    public static Set<SyncRecordEntity> syncRecordsToSyncRecordEntities(List<SyncRecord> syncRecords) {
+        Set<SyncRecordEntity> syncRecordEntities = new HashSet<>();
         for (SyncRecord syncRecord : syncRecords) {
             syncRecordEntities.add(new SyncRecordEntity(syncRecord.getEmployerData().getResourceId().value(), syncRecord.getRecordData().getMessage(), syncRecord.getEmployerData().getEmployeeName(), syncRecord.getErrorCode(), syncRecord.getRecordData().getDate(), syncRecord.getHoursLogged(), syncRecord.getRecordData().getWorkOrder()));
         }
         return syncRecordEntities;
     }
 
-    public static List<SyncDayEntity> syncDaysToSyncDaysEntities(List<SyncDay> syncDays) {
-        List<SyncDayEntity> syncDayEntities = new ArrayList<>();
+    public static Set<SyncDayEntity> syncDaysToSyncDaysEntities(List<SyncDay> syncDays) {
+        Set<SyncDayEntity> syncDayEntities = new HashSet<>();
         for (SyncDay syncDay : syncDays) {
             syncDayEntities.add(new SyncDayEntity(syncDay.getResourceId().value(), syncDay.getDate(), syncDay.getWorkOrder().value(), syncDay.getHoursLoggedCamis(), syncDay.getHoursLoggedTempo()));
         }
